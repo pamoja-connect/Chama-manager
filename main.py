83c99605a3726b3860c84dee5ebcc9b7472f8a3e
pamoja-connect <pamojaagenciesshg@gmail.com>
2025-09-21 14:36:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 
 from app import app
 from routes import *
+from app import app, db
+import models
+
+with app.app_context():
+    db.create_all()
+    print("✅ Database tables created or already exist.")
 
 #if __name__ == '__main__':
    # app.run(host='0.0.0.0', port=5000, debug=True)
